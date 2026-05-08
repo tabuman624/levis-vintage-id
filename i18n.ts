633a75d7bd5bb301577c9a32835802498aeda1d5
tabuman624 <nt.side.624@gmail.com>
2026-05-09 04:39:35 +0900

    fix: remove next-intl from i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -1,13 +1,3 @@
-import { getRequestConfig } from 'next-intl/server';
-
 export const locales = ['ja', 'en'] as const;
 export type Locale = (typeof locales)[number];
 export const defaultLocale: Locale = 'ja';
-
-export default getRequestConfig(async ({ requestLocale }) => {
-  const locale = await requestLocale;
-  return {
-    locale,
-    messages: (await import(`./messages/${locale}.json`)).default,
-  };
-});
